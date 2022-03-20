2190a01fb3ec66ce10c815cd217f2e32a4783a00
Janne Tuukkanen <jatufin@gmail.com>
2022-03-20 09:51:53 +0200

    Rename variables

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,28 +1,28 @@
 class TennisGame:
-    def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+    def __init__(self, playerAName, playerBName):
+        self.playerAName = playerAName
+        self.playerBName = playerBName
+        self.playerAScore = 0
+        self.playerBScore = 0
         self.score_strings = ["Love", "Fifteen", "Thirty", "Forty"]
         
     def won_point(self, player_name):
-        if player_name == self.player1_name:
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.playerAName:
+            self.playerAScore = self.playerAScore + 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.playerBScore = self.playerBScore + 1
 
     def get_score(self):
         score = ""
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 > 3:
+        if self.playerAScore == self.playerBScore:
+            if self.playerAScore > 3:
                 score = "Deuce"
             else:
-                score = self.score_strings[self.m_score1] + "-All"
+                score = self.score_strings[self.playerAScore] + "-All"
 
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+        elif self.playerAScore >= 4 or self.playerBScore >= 4:
+            minus_result = self.playerAScore - self. playerBScore
 
             if minus_result == 1:
                 score = "Advantage player1"
@@ -33,6 +33,6 @@ class TennisGame:
             else:
                 score = "Win for player2"
         else:
-            score = self.score_strings[self.m_score1] + "-" + self.score_strings[self.m_score2]
+            score = self.score_strings[self.playerAScore] + "-" + self.score_strings[self.playerBScore]
 
         return score
