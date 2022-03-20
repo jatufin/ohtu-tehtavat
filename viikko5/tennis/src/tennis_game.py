f551dab4205ddb3efe2c95ceea1ca1e33c6667c5
Janne Tuukkanen <jatufin@gmail.com>
2022-03-20 10:24:07 +0200

    Create and use an inner Player class

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,36 +1,51 @@
 class TennisGame:
-    def __init__(self, playerAName, playerBName):
-        self.playerAName = playerAName
-        self.playerBName = playerBName
-        self.playerAScore = 0
-        self.playerBScore = 0
+    class Player:
+        def __init__(self, name):
+            self._name = name
+            self._score = 0
+
+        def addPoint(self):
+            self._score += 1
+
+        @property
+        def name(self):
+            return self._name
+
+        @property
+        def score(self):
+            return self._score
+        
+    def __init__(self, nameA, nameB):
+        self.playerA = self.Player(nameA)
+        self.playerB = self.Player(nameB)        
+
         self.score_strings = ["Love", "Fifteen", "Thirty", "Forty"]
 
     def won_point(self, player_name):
-        if player_name == self.playerAName:
-            self.playerAScore = self.playerAScore + 1
+        if player_name == self.playerA.name:
+            self.playerA.addPoint()
         else:
-            self.playerBScore = self.playerBScore + 1
+            self.playerB.addPoint()
 
     def get_score(self):
-        if self.playerAScore == self.playerBScore:
-            if self.playerAScore > 3:
+        if self.playerA.score == self.playerB.score:
+            if self.playerA.score > 3:
                 return "Deuce"
             else:
-                return self.score_strings[self.playerAScore] + "-All"
+                return self.score_strings[self.playerA.score] + "-All"
 
-        if self.playerAScore >= 4 or self.playerBScore >= 4:
-            difference = self.playerAScore - self. playerBScore
+        if self.playerA.score >= 4 or self.playerB.score >= 4:
+            difference = self.playerA.score - self. playerB.score
 
             if difference > 0:
-                playerName = self.playerAName
+                playerName = self.playerA.name
             else:
-                playerName = self.playerBName
+                playerName = self.playerB.name
                 
             if abs(difference) == 1:
                 return "Advantage " + playerName
             else:
                 return "Win for " + playerName
 
-        return self.score_strings[self.playerAScore] + "-" + self.score_strings[self.playerBScore]
+        return self.score_strings[self.playerA.score] + "-" + self.score_strings[self.playerB.score]
 
