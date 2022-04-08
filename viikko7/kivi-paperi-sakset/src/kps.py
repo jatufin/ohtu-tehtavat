deb6526bfef610f6002d6251c555635471dd02ee
Janne Tuukkanen <jatufin@gmail.com>
2022-04-09 00:49:45 +0300

    Refactor the game loop

--- viikko7/kivi-paperi-sakset/src/kps.py
+++ viikko7/kivi-paperi-sakset/src/kps.py
@@ -3,15 +3,15 @@ class KPS:
         self._tuomari = tuomari
         
     def pelaa(self):
-        ekan_siirto = self._ensimmaisen_siirto()
-        tokan_siirto = self._toisen_siirto(ekan_siirto)
+        while True:
+            ekan_siirto = self._ensimmaisen_siirto()
+            tokan_siirto = self._toisen_siirto(ekan_siirto)
 
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
+            if self._tuntematon_siirto(ekan_siirto) or self._tuntematon_siirto(tokan_siirto):
+                break
+            
             self._tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
             print(self._tuomari)
-
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            tokan_siirto = self._toisen_siirto(ekan_siirto)
 
         print("Kiitos!")
         print(self._tuomari)
@@ -22,8 +22,11 @@ class KPS:
     def _toisen_siirto(self, ensimmäisen_siirto):
         return "k"
 
-    def _onko_ok_siirto(self, siirto):
-        return siirto == "k" or siirto == "p" or siirto == "s"
+    def _tuntematon_siirto(self, siirto):
+        if len(siirto) != 1:
+            return True
+        
+        return siirto not in "kps"
 
     @staticmethod
     def luo_kaksinpeli(tuomari):
