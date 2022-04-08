f3594dbab59ca79df51ee2b06646fabd757585e4
Janne Tuukkanen <jatufin@gmail.com>
2022-04-08 16:56:44 +0300

    Ex 6.3 Add Or class

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,13 +1,13 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import All, And, Not, HasAtLeast, PlaysIn, HasFewerThan
+from matchers import All, And, Or, Not, HasAtLeast, PlaysIn, HasFewerThan
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    
+
     # matcher = And(
     #     HasAtLeast(5, "goals"),
     #     HasAtLeast(5, "assists"),
@@ -16,14 +16,28 @@ def main():
 
     # for player in stats.matches(matcher):
     #     print(player)
-        
+
+    # matcher = And(
+    #     Not(HasAtLeast(1, "goals")),
+    #     PlaysIn("NYR")
+    # )
+
+    # matcher = Or(
+    #     HasAtLeast(30, "goals"),
+    #     HasAtLeast(50, "assists")
+    # )
+
     matcher = And(
-        Not(HasAtLeast(1, "goals")),
-        PlaysIn("NYR")
+        HasAtLeast(40, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("NYI"),
+            PlaysIn("BOS")
+        )
     )
-    
+
     for player in stats.matches(matcher):
         print(player)
-    
+
 if __name__ == "__main__":
     main()
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -2,16 +2,30 @@ class All:
     def matches(self, player):
         return True
 
+
 class And:
     def __init__(self, *matchers):
         self._matchers = matchers
-    
+
     def matches(self, player):
         for matcher in self._matchers:
             if not matcher.matches(player):
                 return False
-        
+
         return True
+
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def matches(self, player):
+        for matcher in self._matchers:
+            if matcher.matches(player):
+                return True
+
+        return False
+
 
 class Not:
     def __init__(self, matcher):
@@ -19,13 +33,15 @@ class Not:
 
     def matches(self, player):
         return not self._matcher.matches(player)
-    
+
+
 class PlaysIn:
     def __init__(self, team):
         self._team = team
 
     def matches(self, player):
         return player.team == self._team
+
 
 class HasAtLeast:
     def __init__(self, value, attr):
@@ -36,6 +52,7 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
 
 class HasFewerThan:
     def __init__(self, value, attr):
