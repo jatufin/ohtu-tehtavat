bc68699c23a27b55fa3e4b20b98c55878133ec7d
Janne Tuukkanen <jatufin@gmail.com>
2022-04-09 13:57:03 +0300

    Rename all IO things to Pelaaja things

--- viikko7/kivi-paperi-sakset/src/komennot.py
+++ viikko7/kivi-paperi-sakset/src/komennot.py
@@ -2,11 +2,11 @@ from kps import KPS
 
 
 class Komennot:
-    def __init__(self, eka_io, toka_io, tekoaly_io, parempi_tekoaly_io, tuomari):
+    def __init__(self, eka, toka, tekoaly, parempi_tekoaly, tuomari):
         self._komennot = {
-            "a": Peli(eka_io, toka_io, tuomari),
-            "b": Peli(eka_io, tekoaly_io, tuomari),
-            "c": Peli(eka_io, parempi_tekoaly_io, tuomari)
+            "a": Peli(eka, toka, tuomari),
+            "b": Peli(eka, tekoaly, tuomari),
+            "c": Peli(eka, parempi_tekoaly, tuomari)
         }
 
     def suorita(self, komento):
@@ -16,26 +16,24 @@ class Komennot:
         self._komennot[komento].aloita()
 
         return True
-    
-    def valikko(self, io):
+
+    def valikko(self, pelaaja):
         teksti = """
 Valitse pelataanko
 (a) Ihmistä vastaan
 (b) Tekoälyä vastaan
 (c) Parannettua tekoälyä vastaan
 Muilla valinnoilla lopetetaan"""
-        io.kirjoita(teksti)
+        pelaaja.kirjoita(teksti)
 
-        return io.lue()
+        return pelaaja.lue()
 
 
 class Peli:
-    def __init__(self, eka_io, toka_io, tuomari):
-        self._eka_io = eka_io
-        self._toka_io = toka_io
+    def __init__(self, eka, toka, tuomari):
+        self._eka = eka
+        self._toka = toka
         self._tuomari = tuomari
 
     def aloita(self):
-        KPS.luo_peli(self._eka_io, self._toka_io, self._tuomari).pelaa()
-
-
+        KPS.luo_peli(self._eka, self._toka, self._tuomari).pelaa()
--- viikko7/kivi-paperi-sakset/src/kps.py
+++ viikko7/kivi-paperi-sakset/src/kps.py
@@ -1,12 +1,12 @@
 class KPS:
-    def __init__(self, eka_io, toka_io, tuomari):
-        self._eka_io = eka_io
-        self._toka_io = toka_io
+    def __init__(self, eka, toka, tuomari):
+        self._eka = eka
+        self._toka = toka
         self._tuomari = tuomari
-        
+
     def pelaa(self):
-        self._eka_io.kirjoita("Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s")
-        
+        self._eka.kirjoita("Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s")
+
         while True:
             ekan_siirto = self._ensimmaisen_siirto()
             tokan_siirto = self._toisen_siirto(ekan_siirto)
@@ -14,31 +14,31 @@ class KPS:
             if (self._tuntematon_siirto(ekan_siirto) or
                 self._tuntematon_siirto(tokan_siirto)):
                 break
-            
-            self._tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            self._eka_io.kirjoita(self._tuomari)
 
-        self._eka_io.kirjoita("Kiitos!")
-        self._eka_io.kirjoita(self._tuomari)
-        
+            self._tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
+            self._eka.kirjoita(self._tuomari)
+
+        self._eka.kirjoita("Kiitos!")
+        self._eka.kirjoita(self._tuomari)
+
     def _ensimmaisen_siirto(self):
-        return self._eka_io.lue("Ensimmäisen pelaajan siirto: ")
+        return self._eka.lue("Ensimmäisen pelaajan siirto: ")
 
     def _toisen_siirto(self, ensimmaisen_siirto):
-        tokan_siirto = self._toka_io.lue("Toisen pelaajan siirto: ")
-        self._toka_io.kirjoita(ensimmaisen_siirto)
+        tokan_siirto = self._toka.lue("Toisen pelaajan siirto: ")
+        self._toka.kirjoita(ensimmaisen_siirto)
 
-        if self._toka_io.tyyppi == "tietokone":
-            self._eka_io.kirjoita(f"Tietokone valitsi: {tokan_siirto}")
-        
+        if self._toka.tyyppi == "tietokone":
+            self._eka.kirjoita(f"Tietokone valitsi: {tokan_siirto}")
+
         return tokan_siirto
 
     def _tuntematon_siirto(self, siirto):
         if len(siirto) != 1:
             return True
-        
+
         return siirto not in "kps"
 
     @staticmethod
-    def luo_peli(eka_io, toka_io, tuomari):
-        return KPS(eka_io, toka_io, tuomari)
+    def luo_peli(eka, toka, tuomari):
+        return KPS(eka, toka, tuomari)
